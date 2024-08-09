165ba2ddd42c151ad97a44c07171365599d44d2f
Tom C <tom.carlier@epitech.eu>
2024-08-09 07:15:26 +0200

    feat (UserREpository): add Login and register route to the repositry

--- Client/Repository/Interface/IUsersRepository.cs
+++ Client/Repository/Interface/IUsersRepository.cs
@@ -5,4 +5,8 @@ namespace Client.Repository.Interface;
 public interface IUsersRepository
 {
     public Task<List<UserModel>> GetUsers();
+
+    public Task<UserModel> Login(string username, string password);
+    
+    public Task<UserModel> Register(string username, string password);
 }
--- Client/Repository/UserRepository.cs
+++ Client/Repository/UserRepository.cs
@@ -8,6 +8,24 @@ public class UserRepository(HttpClient client) : BaseRepository(client), IUsersR
     private static readonly string Url = "api/User/";
 
     private static readonly string UrlGetUsers = Url + "GetUsers";
+    
+    private static readonly string UrlLogin = Url + "Login";
+
+    private static readonly string UrlRegister = Url + "Register";
 
     public async Task<List<UserModel>> GetUsers() => await Get<List<UserModel>>(UrlGetUsers);
+
+    public async Task<UserModel> Login(string username, string password) =>
+        await Post(UrlLogin, new UserModel
+        {
+            Username = username,
+            Password = password
+        });
+
+    public async Task<UserModel> Register(string username, string password) =>
+        await Post(UrlRegister, new UserModel
+        {
+            Username = username, 
+            Password = password
+        });
 }
