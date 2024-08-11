87dfbfe7d01b3d1fdd2a9d448986606019d2992e
Tom C <tom.carlier@epitech.eu>
2024-08-11 22:20:11 +0200

    refactor (run sh): change a thing  in the build script

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 BASE_DIR="/home/fu/code/App"
 
 # Build each project
-echo "Building the Server project..."
-cd $BASE_DIR/Server && dotnet build
-
 echo "Building the Client project..."
 cd $BASE_DIR/Client && dotnet build
 
 echo "Building the Shared project..."
 cd $BASE_DIR/Shared && dotnet build
+
+echo "Building the Server project..."
+cd $BASE_DIR/Server && dotnet build
 
 kitty --hold sh -c "cd /home/fu/code/App/Server && dotnet run" & disown
 cd /home/fu/code/App/Client && dotnet run
