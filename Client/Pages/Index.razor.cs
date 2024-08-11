096d2e335de411eb9104d5c0e851516b49dd5cb2
Tom C <tom.carlier@epitech.eu>
2024-08-11 22:24:48 +0200

    del (index.cs): remove test code

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -10,12 +10,4 @@ namespace Client.Pages;
 // ReSharper disable once ClassNeverInstantiated.Global
 public partial class Index : ComponentBase
 {
-    [Inject] public required IUsersRepository UsersRepository { get; set; }
-
-    protected override async Task OnInitializedAsync()
-    {
-        var tmp = await UsersRepository.Register("admin4", "admin4");
-        Console.WriteLine("Result: " +  tmp.ToJson());
-        await base.OnInitializedAsync();
-    }
 }
