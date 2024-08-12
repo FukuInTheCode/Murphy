d4f2b487236184a80163c409c571664bf61b312b
Tom C <tom.carlier@epitech.eu>
2024-08-12 21:26:03 +0200

    chore (auth state providr): add custom auth state provider services

---
+++ Client/Services/MurphyAuthentificationState.cs
@@ -0,0 +1,55 @@
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
+namespace Client.Services;
+
+public class MurphyAuthenticationStateProvider(IJSRuntime jsRuntime) : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "authToken");
+
+        var identity = string.IsNullOrEmpty(token)
+            ? new ClaimsIdentity()
+            : new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+
+        return new AuthenticationState(new ClaimsPrincipal(identity));
+    }
+
+    public void MarkUserAsAuthenticated(string token)
+    {
+        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+        var user = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public void MarkUserAsLoggedOut()
+    {
+        var identity = new ClaimsIdentity();
+        var user = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+        return keyValuePairs != null ? keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString() ?? throw new InvalidOperationException())).ToList() : [];
+    }
+
+    private byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+        return Convert.FromBase64String(base64);
+    }
+}
