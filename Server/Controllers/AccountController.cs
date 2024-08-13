bc55f07c346011897928aff0dd875a7ff3ce31f8
Tom C <tom.carlier@epitech.eu>
2024-08-13 22:17:47 +0200

    feat (account controller): add a jwt generator

--- Server/Controllers/AccountController.cs
+++ Server/Controllers/AccountController.cs
@@ -1,18 +1,22 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Bson;
 using Server.DataAccess;
-using Shared.Helpers.User;
+using Shared.Helpers.Account;
 using Shared.Model.Users;
 
 namespace Server.Controllers;
 
 [Controller]
 [Route("api/[controller]")]
-public class AccountController : BaseController
+public class AccountController(IConfiguration configuration) : BaseController
 {
     private readonly AccountMongoAccess _accountMongoAccess = new();
-    
+
     #region Get
 
     [HttpGet]
@@ -46,6 +50,32 @@ public class AccountController : BaseController
         await _accountMongoAccess.CreateUser(accountInfo);
         accountInfo.Password = string.Empty;
         return Created(accountInfo);
+    }
+
+    #endregion
+
+    #region Utils
+
+    public string GenerateJwt(AccountInfoModel accountInfo)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, accountInfo.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        new Claim(ClaimTypes.Role, "Admin")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: configuration["Jwt:Issuer"],
+            audience: configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
     }
 
     #endregion
