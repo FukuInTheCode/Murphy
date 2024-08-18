05c694ca7b1ca2ab11912e1038c7c9a3b917702a
Tom C <tom.carlier@epitech.eu>
2024-08-18 17:05:09 +0200

    chore (constantes): add color constantes

---
+++ Shared/Constantes/ColorConstantes.cs
@@ -0,0 +1,17 @@
+namespace Shared.Constantes;
+
+public static class ColorConstantes
+{
+    public const string Sapphire = "#164dad";
+
+    public const string ChineseRed = "#BD5545";
+
+    public const string WageningenGreen = "#16AD2B";
+
+    public const string Turquoise = "#4bf0d7";
+
+    public const string White = "#ffffff";
+
+    public const string Black = "#000000";
+
+}
